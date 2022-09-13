5034119cd2fc8654f366ddcfd18b424cb0b711cc
Ilshat Gelmutdinov <gelmutdinov1988@mail.ru>
2022-09-13 17:01:29 +0300

    Added class TaskWriteToFile

--- src/ru/ilshat/TaskWriteToFile.java
+++ src/ru/ilshat/TaskWriteToFile.java
@@ -2,18 +2,23 @@ package ru.ilshat;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
-public class ReadingFile {
-
+public class TaskWriteToFile {
     public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("input.txt");
-        Scanner scanner = new Scanner(file);
+
+        File inputFile = new File("input.txt");
+        Scanner scanner = new Scanner(inputFile);
         String line = scanner.nextLine();
         String[] numbers = line.split(" ");
         scanner.close();
         double x = 0.0;
         double y = 0.0;
+        double z = 0.0;
+
+        File outputFile = new File("output.txt");
+        PrintWriter printWriter = new PrintWriter(outputFile);
 
         try {
             x = Double.parseDouble(numbers[0]);
@@ -22,17 +27,21 @@ public class ReadingFile {
             switch (numbers[1]) {
                 case "+":
                     System.out.println(x + y);
+                    z = x + y;
                     break;
                 case "-":
                     System.out.println(x - y);
+                    z = x - y;
                     break;
                 case "/":
                     if (y == 0)
                         System.out.print("Error! Division by zero");
                     else System.out.println(x / y);
+                    z = x / y;
                     break;
                 case "*":
                     System.out.println(x * y);
+                    z = x * y;
                     break;
                 default:
                     System.out.println("Operation Error!");
@@ -41,9 +50,11 @@ public class ReadingFile {
             System.out.println("Error! Not number\n");
         }
 
+
         scanner.close();
+        printWriter.println(z); // записываем ответ в файл
+        printWriter.close();
+
     }
-
 }
-
 
